a175b0ad5d0aa520bb3b9e159d88a20573d23562
Muhammad Noman <mhmdnoman01@gmail.com>
2025-09-17 23:46:47 +0600

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Suspense } from "react";
+import Countries from "./components/Countries/Countries";
+
+const countriesPromise = fetch(
+  "https://restcountries.com/v3.1/all?fields=name,capital,currencies"
+).then((res) => res.json());
+
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Suspense fallback={<h3>Countries List</h3>}>
+        <Countries countriesPromise={countriesPromise} />
+      </Suspense>
     </div>
   );
 }
